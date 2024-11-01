bc415f360cbbe8e52ae4330826fa1b095b82d576
Seiji Tanimoto <72504808+seiji1997@users.noreply.github.com>
2024-11-01 16:16:55 +0900

    Update note.py

--- AWS/Lambda/note.py
+++ AWS/Lambda/note.py
@@ -1,1 +1,11 @@
+import polars as pl
 
+df = pl.read_parquet("your_file.parquet")
+
+df = df.with_columns(
+    pl.col("column_name")
+    .str.replace_all(r'^"(.*)"$', r'\1')
+    .cast(pl.Utf8)
+)
+
+print(df)
